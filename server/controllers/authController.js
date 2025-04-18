a9b3750cdd8977a59b669a034badfc1107477c26
Hardik Agarwal <hardikagarwal9308@gmail.com>
2025-04-18 18:24:09 +0530

    fix: errors fixed

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -10,13 +10,13 @@ export const register = async (req, res) => {
   }
 
   try {
-    const existingUser = await userModel.findOne(email);
+    const existingUser = await userModel.findOne({ email });
 
     if (existingUser) {
       return res.json({ success: false, message: "User Already Exists" });
     }
 
-    const hashedPassword = bcrypt.hash(password, 10);
+    const hashedPassword = await bcrypt.hash(password, 10);
 
     const user = new userModel({ name, email, password: hashedPassword });
     await user.save();
@@ -26,7 +26,7 @@ export const register = async (req, res) => {
     });
 
     res.cookie("token", token, {
-      httpOny: true,
+      httpOnly: true,
       secure: process.env.NODE_ENV === "development",
       sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
       maxAge: 7 * 24 * 60 * 60 * 1000,
@@ -60,7 +60,7 @@ export const login = async (req, res) => {
     });
 
     res.cookie("token", token, {
-      httpOny: true,
+      httpOnly: true,
       secure: process.env.NODE_ENV === "development",
       sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
       maxAge: 7 * 24 * 60 * 60 * 1000,
@@ -74,7 +74,7 @@ export const login = async (req, res) => {
 export const logout = async (req, res) => {
   try {
     res.clearCookie("token", {
-      httpOny: true,
+      httpOnly: true,
       secure: process.env.NODE_ENV === "development",
       sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
       maxAge: 7 * 24 * 60 * 60 * 1000,
