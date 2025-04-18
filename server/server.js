e529fe80cafc13e6bb665e3ca6d881cf2a379d85
Hardik Agarwal <hardikagarwal9308@gmail.com>
2025-04-18 18:11:31 +0530

    Api Working

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
 import connectDatabase from "./config/connectDb.js";
+import authRouter from "./routes/authRoutes.js";
 
 const app = express();
 
@@ -13,9 +14,11 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ credentials: true }));
 
+//Api Endpoints
 app.get("/", (req, res) => {
   res.send("Api Working fine");
 });
+app.use("/api/auth", authRouter);
 
 app.listen(port, () => {
   console.log(`Server Started on Port ${port}`);
