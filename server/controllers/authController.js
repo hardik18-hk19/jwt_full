721bc94c7f6534ec2e9da6b16f6be5583292beb0
Hardik Agarwal <hardikagarwal9308@gmail.com>
2025-04-19 19:21:11 +0530

    Email Sending Done

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,7 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import userModel from "../models/user.model.js";
+import transporter from "../config/nodemailer.js";
 
 export const register = async (req, res) => {
   const { name, email, password } = req.body;
@@ -31,6 +32,22 @@ export const register = async (req, res) => {
       sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
+
+    const mailOptions = {
+      from: process.env.SENDER_EMAIL,
+      to: email,
+      subject: "Welcome To HK Auth",
+      text: `Welcome to  my auth. Your Account has been created with email: ${email}`,
+    };
+
+    try {
+      const info = await transporter.sendMail(mailOptions);
+      // console.log("✅ Email sent:", info.response);
+    } catch (err) {
+      console.error("❌ Email sending failed:", err.message);
+    }
+
+    return res.json({ success: true });
   } catch (error) {
     return res.json({ success: false, message: error.message });
   }
