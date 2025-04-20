66b6ad44f82f99b3e2700a9993dd9bd97604aac8
Hardik Agarwal <hardikagarwal9308@gmail.com>
2025-04-20 20:23:45 +0530

    Routes added for password reset and otp for reset

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -3,8 +3,10 @@ import {
   login,
   logout,
   register,
+  sendPasswordResetOtp,
   verifyEmail,
   verifyOtp,
+  verifyPasswordReset,
 } from "../controllers/authController.js";
 import userauth from "../middleware/userAuth.js";
 
@@ -15,5 +17,7 @@ authRouter.post("/login", login);
 authRouter.post("/logout", logout);
 authRouter.post("/send-verify-otp", userauth, verifyOtp);
 authRouter.post("/verify-account", userauth, verifyEmail);
+authRouter.post("/send-reset-otp", userauth, sendPasswordResetOtp);
+authRouter.post("/password-reset", userauth, verifyPasswordReset);
 
 export default authRouter;
